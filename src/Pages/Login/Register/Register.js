92d95f6df3092a4581b61b85e1538d22482326f3
sadiqss <sadiq.sau48@gmail.com>
2022-05-02 06:26:14 +0100

    user registered

--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import { useForm } from "react-hook-form";
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import auth from '../../../firebase.init';
 
 const Register = () => {
-    const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const handleRegister = async (event) => {
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        await createUserWithEmailAndPassword(email, password);
+    }
     return (
-        <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("firstName", { required: true })} />
-            <input {...register("lastName")} />
-            <input type="number" {...register("age", { min: 18, max: 99 })} />
-            <input type="submit" />
+        <form onSubmit={handleRegister}>
+            <input type="text" name="name" id="" placeholder='Your Name' />
+
+            <input type="email" name="email" id="" placeholder='Email Address' required />
+
+            <input type="password" name="password" id="" placeholder='Password' required />
+            <input
+                className='w-50 mx-auto btn btn-primary mt-2'
+                type="submit"
+                value="Register" />
         </form>
     );
 };
