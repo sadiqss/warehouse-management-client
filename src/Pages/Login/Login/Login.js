ea82b2d765efd33feb9a4583576d11a2eb334242
sadiqss <sadiq.sau48@gmail.com>
2022-05-03 18:10:58 +0100

    login

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import PageTitle from '../../Shared/PageTitle/PageTitle';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passRef = useRef('');
     const navigate = useNavigate();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
     const handleSubmit = async event => {
         event.preventDefault();
+        const email = emailRef.current.value;
+        const pass = passRef.current.value;
+        await signInWithEmailAndPassword(email, pass);
     }
     const navigateRegister = event => {
         navigate('/register');
@@ -18,10 +32,10 @@ const Login = () => {
                 <h2 className='text-primary text-center mt-2'>Please Login</h2>
                 <Form onSubmit={handleSubmit}>
                     <Form.Group className="mb-3" controlId="formBasicEmail">
-                        <Form.Control type="email" placeholder="Enter email" required />
+                        <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                     </Form.Group>
                     <Form.Group className="mb-3" controlId="formBasicPassword">
-                        <Form.Control type="password" placeholder="Password" required />
+                        <Form.Control ref={passRef} type="password" placeholder="Password" required />
                     </Form.Group>
                     <Button variant="primary w-50 mx-auto d-block mb-2" type="submit">
                         Login
